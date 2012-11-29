9224cf15b616ba18498f9bd08a008cbb5cd3e0db
Bill Prickett <bprickett@mac.com>
2012-11-29 15:32:44 -0500

    Nav with Photos and Music
    Custom Nav with Photos and Music
    Added Mediaelement-JS music player

--- wp-content/themes/BillsTestBox/functions.php
+++ wp-content/themes/BillsTestBox/functions.php
@@ -485,6 +485,34 @@ add_filter( 'attachment_link', 'toolbox_enhanced_image_navigation' );
 	
 	// END BILLs custom Catagory code
 	
+
+	function my_custom_post_product() {
+		$labels = array(
+			'name'               => _x( 'Products', 'post type general name' ),
+			'singular_name'      => _x( 'Product', 'post type singular name' ),
+			'add_new'            => _x( 'Add New', 'book' ),
+			'add_new_item'       => __( 'Add New Product' ),
+			'edit_item'          => __( 'Edit Product' ),
+			'new_item'           => __( 'New Product' ),
+			'all_items'          => __( 'All Products' ),
+			'view_item'          => __( 'View Product' ),
+			'search_items'       => __( 'Search Products' ),
+			'not_found'          => __( 'No products found' ),
+			'not_found_in_trash' => __( 'No products found in the Trash' ), 
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Products'
+		);
+		$args = array(
+			'labels'        => $labels,
+			'description'   => 'Holds our products and product specific data',
+			'public'        => true,
+			//'menu_position' => 9,
+			'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+			'has_archive'   => true,
+		);
+		register_post_type( 'product', $args );	
+	}
+	add_action( 'init', 'my_custom_post_product' );
 	
 	
 	
